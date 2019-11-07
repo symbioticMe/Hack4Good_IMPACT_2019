7cc2b215c8c5a0abcf2fcf6693800a21d17b90ec
Andrei Kolar <andreikolartoronto@gmail.com>
2019-11-07 20:41:42 +0000

    Upload New File

---
+++ src/filter_on_time_window.R
@@ -0,0 +1,15 @@
+# INPUTS: 
+# data - output of the function 'complete_data'
+# time_window - an integer for the number of months to be analyzed
+# final_month - last month to be analyzed (in yearmon format)
+
+# OUTPUT:
+# data only filtered to include the months desired for analysis
+
+filter_on_time_window<-function(data, time_window, final_month){
+  
+  beginning_month <- final_month-((time_window-1)/12)
+  filtered_data <- data[data$Month>=beginning_month & data$Month<= final_month,]
+  
+  return(filtered_data)
+}
