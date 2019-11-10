3304137d635905edaf11eae862b9dfffc6fdb3c0
Jelena Čuklina <chuklina.jelena@gmail.com>
2019-11-10 12:47:00 +0100

    splitting get_label into two functions

--- src/get_label_function.R
+++ src/get_label_function.R
@@ -3,12 +3,13 @@
 #' 
 #' @return a label which classifies the price of a commoditiy as stable, trend, volatile or volatile trend
 #' @inheritParams indicators
-get_label<- function(df, final_month, time_window=6, frac_missing=0.2, 
-                     geo_level = 'q_district', commodity_type = 'smeb_total_float'){
-  # Step 0
+
+library(tidyverse)
+
+get_aggregated_table <- function(){
   source("src/aggregate_price_geogr.R")
   df <- aggregate_price_geogr(df, geo_level, commodity_type)
-    
+  
   # take df and output df_aggr and complete it
   source('src/complete_data.R')
   df <- complete_data(df)
@@ -16,6 +17,12 @@ get_label<- function(df, final_month, time_window=6, frac_missing=0.2,
   #filter on time window
   source('src/filter_on_time_window.R')
   df <- filter_on_time_window(data = df, time_window = time_window, final_month = final_month)
+}
+
+get_label<- function(df, final_month, time_window=6, frac_missing=0.2, 
+                     geo_level = 'q_district', commodity_type = 'smeb_total_float'){
+  # Step 0
+ 
   
   # return a price vector
   
@@ -28,14 +35,17 @@ get_label<- function(df, final_month, time_window=6, frac_missing=0.2,
   for (location in geogr_units){
     
     # Step 1
+    df_price_vector = df[which(df[[geo_level]] == location), ]
+    price_vector = df_price_vector[[commodity_type]]
+    
     derivative_vector <- get_derivative(price_vector)
     # Step 2
     source("src/aggregate_quantity.R")
-    aggr_stats <- aggregate_quantity(price_vector, derivative_vector)
+    aggr_stats <- aggregate_quantity(derivative_vector)
     sd_der = aggr_stats$sd
     mean_der = aggr_stats$mean
     n_missing_der = aggr_stats$n_missing
-    frac_missing_der = aggr_statsfrac_missing
+    frac_missing_der = aggr_stats$frac_missing
     n_der = aggr_stats$n
     
     new_df = data.frame(location = location, sd_der = sd_der, mean_der = mean_der, 
@@ -46,7 +56,7 @@ get_label<- function(df, final_month, time_window=6, frac_missing=0.2,
     
     # Step 3
     # source("src/assign_label.R")
-    # new_df$label <- assign_label(new_df, thres=NULL)
+    # new_df$label <- assign_label(new_df, thres=NULL, frac_missing = frac_missing)
     # return label
     label_df_list[[location]] = new_df
   }
